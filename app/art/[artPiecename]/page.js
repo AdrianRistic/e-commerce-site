ccca1de0a8d0f679e60e9fa719f82d836b18a319
Adrian N. Ristic <toffees.taco.0f@icloud.com>
2023-02-02 12:16:43 +0100

    Added pictures to artpiece pages

--- app/art/[artPiecename]/page.js
+++ app/art/[artPiecename]/page.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { artPieces } from '../../../database/artpieces';
 
 export const dynamic = 'force-dynamic';
@@ -16,6 +18,15 @@ export default function Arts(props) {
       <main>
         This is a {singleartPiece.type} in the {singleartPiece.style} - Style
       </main>
+
+      <Link href={`/art/${singleartPiece.name.toLocaleLowerCase()}`}>
+        <Image
+          src={`/images/${singleartPiece.name}-${singleartPiece.id}.png`}
+          alt={singleartPiece.type}
+          width="200"
+          height="200"
+        />
+      </Link>
     </>
   );
 }
