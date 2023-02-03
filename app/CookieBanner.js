ce0c8ce05a04affbbbde5a84508f0caf05b73a89
Adrian N. Ristic <toffees.taco.0f@icloud.com>
2023-02-03 15:34:50 +0100

    Added Cookie Accept Cookies

--- app/CookieBanner.js
+++ app/CookieBanner.js
@@ -2,14 +2,31 @@
 import { useState } from 'react';
 
 export default function CookieBanner() {
-  const [isCookieBannerVisible, setIsCookieBannerVisible] = useState(true);
+  const localStorageValue = JSON.parse(
+    window.localStorage.getItem('areCookieTermsAccepted'),
+  );
+
+  const initialState = localStorageValue === null ? false : localStorageValue;
+
+  const [areCookieTermsAccepted, setAreCookieTermsAccepted] =
+    useState(initialState);
   return (
-    isCookieBannerVisible && (
+    !areCookieTermsAccepted && (
       <>
         <div>
           We use cookies on this site. Please accept the terms and conditions.
         </div>
-        <button onClick={() => setIsCookieBannerVisible(false)}>Accept</button>
+        <button
+          onClick={() => {
+            setAreCookieTermsAccepted(true);
+            window.localStorage.setItem(
+              'areCookieTermsAccepted',
+              JSON.stringify(true),
+            );
+          }}
+        >
+          Accept
+        </button>
       </>
     )
   );
