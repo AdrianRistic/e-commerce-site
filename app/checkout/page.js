e982a611b290cd22731ea57655ac7ff81123580b
Adrian N. Ristic <toffees.taco.0f@icloud.com>
2023-02-12 14:45:25 +0100

    Add checkout fields

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -2,6 +2,27 @@ export default function checkOut() {
   return (
     <>
       <h1>Checkout:</h1>
+      <p>First Name:</p>
+      <input></input>
+      <p> Last Name:</p>
+      <input></input>
+      <p>E-Mail:</p>
+      <input></input>
+      <p>Adress:</p>
+      <input></input>
+      <p>City:</p>
+      <input></input>
+      <p>Postal Code:</p>
+      <input></input>
+      <p>Country:</p>
+      <input></input>
+      <p>Credit Card Number:</p>
+      <input></input>
+      <p>Credit Card Expiration Date:</p>
+      <input></input>
+      <p>Credit Card Security Code:</p>
+      <input></input>
+      <button>Confirm order</button>
     </>
   );
 }
